052f56a880a6fb7aa6fb6aff96b19abc0d6b89c9
Rishu jha <jharishu796@gmail>
2023-02-19 09:01:51 +0530

    fix

--- src/redux/cart/cart.api.ts
+++ src/redux/cart/cart.api.ts
@@ -2,12 +2,12 @@ import { Cart } from "@/utils/types";
 import axios, { AxiosResponse } from "axios"
 
 export const getCartAPI = async () =>{
-    let res: AxiosResponse<Cart[]> = await axios.get('http://localhost:8080/carts');
+    let res: AxiosResponse<Cart[]> = await axios.get('https://dead-gray-bass-fez.cyclic.app/carts');
     return res.data;
 }
 
 export const addItemToCartAPI = async (productId:number,quantity:number) =>{
-    let res: AxiosResponse<Cart> = await axios.post('http://localhost:8080/carts',{productId,quantity});
+    let res: AxiosResponse<Cart> = await axios.post('https://dead-gray-bass-fez.cyclic.app/carts',{productId,quantity});
     return res.data;
 };
 
--- src/redux/product/product.api.ts
+++ src/redux/product/product.api.ts
@@ -2,13 +2,13 @@ import { Product } from "@/utils/types";
 import axios, {AxiosResponse} from "axios"
 
 export const getProductsAPI = async ()=>{
-    let res: AxiosResponse<Product[]> = await axios.get('http://localhost:8080/products');
+    let res: AxiosResponse<Product[]> = await axios.get('https://dead-gray-bass-fez.cyclic.app/products');
     return res.data;
 }
 
 
 export const getProductAPI = async (id:string) => {
-    let res: AxiosResponse<Product> = await axios.get(`http://localhost:8080/products/${id}`);
+    let res: AxiosResponse<Product> = await axios.get(`https://dead-gray-bass-fez.cyclic.app/products/${id}`);
     return res.data;
 }
 
